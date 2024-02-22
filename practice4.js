1a41e97eafdc562ebe4b707de2cddde2186671eb
shafaet.hossain@g.bracu.ac.bd <shafaet.hossain@g.bracu.ac.bd>
2024-02-22 21:57:28 +0600

    little changes

--- practice4.js
+++ practice4.js
@@ -1,11 +1,7 @@
-const twoArrays=(array1,array2) =>{
-return [...array1, ...array2];
-  
+const createArray = (...elements) => elements;
+const Arr1 =createArray(1,2,3);
+const Arr2=createArray(4,5,6);
 
-} 
-const Arr1 =[1,2,3];
-const Arr2=[4,5,6];
-
-const mergedArray=twoArrays(Arr1,Arr2);
+const mergedArray=[...Arr1,...Arr2];
 const arrayMax=Math.max(...mergedArray);
 console.log(arrayMax);
